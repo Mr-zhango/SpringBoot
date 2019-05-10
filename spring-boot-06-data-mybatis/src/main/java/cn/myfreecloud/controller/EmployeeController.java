4f7bfafd7719f72a42cec7edf118e4e2233ebeb4
zhangyang <zhangyang@ec.e-chemchina.com>
2019-05-10 11:07:08 +0800

    add SpringBoot.md & add comment

--- spring-boot-06-data-mybatis/src/main/java/cn/myfreecloud/controller/EmployeeController.java
+++ spring-boot-06-data-mybatis/src/main/java/cn/myfreecloud/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import cn.myfreecloud.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -35,7 +36,7 @@ public class EmployeeController {
      * @param employee
      * @return Integer
      */
-    @GetMapping("/emp")
+    @PostMapping("/emp")
     public Integer insertEmp(Employee employee){
         return employeeMapper.insertEmp(employee);
     }
